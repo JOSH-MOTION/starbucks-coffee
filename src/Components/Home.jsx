e1a704c6be4db2a5e71454a0c7d7c74884b2c9e0
JOSH <joshuadoe168@gmail>
2023-01-20 15:26:55 +0000

    second commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,13 +5,15 @@ import pic3 from './images/pic3.png'
 import pic4 from './images/pic4.png'
 import pic5 from './images/pic5.png'
 import pic6 from './images/pic6.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSpotify , faFacebook, faPinterest, faInstagram, faYoutube, faTwitter, faInstagramSquare } from "@fortawesome/free-brands-svg-icons"
 
 
 
  function Home() {
   return (
-   
-    <div className='container-fluid '>
+   <>
+    <div className='container-fluid all'>
       <div className='row box'>
         <div className='col-md 6 box1' >
         <img src={pic3} className="Aff" alt="pic3" />
@@ -68,13 +70,113 @@ import pic6 from './images/pic6.png'
     
   </div>
   </div>
-  
-   
+  <div className='row'>
+   <div className=' col-md 12 boxx4'>
+     <h3>Start fresh with Starbucks<sup>®</sup> Rewards</h3>
+     <p>Join today to earn your way to free coffee and treats.<sup>*</sup></p>
+   </div>
+   </div>
     
- 
+    </div>
+
+    <div className='footer'>
+      <div className='container-fluid'>
+        <div className='col-md-12'>
+
+        </div>
+        <div className='row'>
+          <div className='col-md-2 first'>
+              <h3>About Us</h3>
+              <ul>
+                <li>Our Company</li>
+                <li>Our Coffee</li>
+                <li>Stories and News</li>
+                <li>Starbucks Archive</li>
+                <li>Investor Relations</li>
+                <li>Customer Service</li>
+
+              </ul>
+          </div>
+         
+          <div className='col-md-2 second'>
+              <h3>Careers</h3>
+              <ul>
+                <li>Culture and Values</li>
+                <li>Inclusion,Diversity and Equity</li>
+                <li>College Achievement Plan</li>
+                <li>Alumni Community</li>
+                <li>U.S Careers</li>
+                <li>International Careers</li>
+
+              </ul>
+          </div>
+
+        
+          
+            <div className='col-md-2 third'>
+              <h3>Social Impact</h3>
+              <ul>
+                <li>People</li>
+                <li>Planet</li>
+                <li>Environmental and Social Impact Reporting</li>
+                
+
+              </ul>
+          </div>
+          
+          <div className='col-md-2 fourth'>
+          
+              <h3>For Business Partners</h3>
+              <ul>
+                <li>Landlord Support Center</li>
+                <li>Suppliers</li>
+                <li>Coporate Gift Card Sales</li>
+                <li>Office and Fodservice Coffee</li>
+                <li>Investor Relations</li>
+                <li>Customer Service</li>
+
+              </ul>
+          </div>
+          
+         
+          <div className='col-md-3 fifth'>
+              <h3>About Us</h3>
+              <ul>
+                <li>Our Company</li>
+                <li>Our Coffee</li>
+                <li>Stories and News</li>
+                <li>Starbucks Archive</li>
+                <li>Investor Relations</li>
+                <li>Customer Service</li>
+
+              </ul>
+          </div>
+          
+          
+          
+          
+        </div>
+
+      </div>
+      <center>
+      <hr />
+      </center>
+
+      <div className='social'>
+        <ul>
+      <li><FontAwesomeIcon icon={faSpotify} /></li>
+      <li><FontAwesomeIcon icon={faFacebook} /></li>
+      <li><FontAwesomeIcon icon={faPinterest} /></li>
+      <li><FontAwesomeIcon icon={faInstagramSquare} /></li>
+      <li><FontAwesomeIcon icon={faYoutube} /></li>
+      <li><FontAwesomeIcon icon={faTwitter} /></li>
+      </ul>
+      </div>
+        
+      
 
     </div>
-    
+    </>
   );
 }
 
